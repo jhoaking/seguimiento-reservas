0a470f6596e802c7572d3c81015b4f06a7108ce4
jhoaking <peredoroj@gmail.com>
2025-08-08 19:14:38 -0400

    aagregando url para webhooks

--- backend/src/interacciones/adapter/n8n-adapter.ts
+++ backend/src/interacciones/adapter/n8n-adapter.ts
@@ -2,11 +2,14 @@ import axios from 'axios';
 import { CreateInteraccioneDto } from '../dto/create-interaccione.dto';
 
 export const sendMessageByN8n = async (payload: CreateInteraccioneDto) => {
-  const urls = [""];
+  const urls = [
+    'https://primary-production-3577.up.railway.app/webhook-test/e758b21c-c7ca-4fa5-b78c-ef00e65d817d',
+    'https://primary-production-3577.up.railway.app/webhook/e758b21c-c7ca-4fa5-b78c-ef00e65d817d',
+  ];
 
   for (const url of urls) {
     try {
-      await axios.post(url,payload);
+      await axios.post(url, payload);
       console.log(`Mensaje enviado correctamente a ${url}`);
     } catch (error) {
       console.error(`Error al enviar a ${url}:`, error.message);
