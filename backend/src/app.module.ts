e4499ec1e20398a5adedaa7aebc52131cbbce08f
jhoaking <peredoroj@gmail.com>
2025-08-15 11:12:15 -0400

    ahora si creo que si

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -19,10 +19,16 @@ import { InteraccionesModule } from './interacciones/interacciones.module';
       },
       type: 'postgres',
 
-      url:process.env.URL ,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
       logging: true,
+      retryAttempts: 10,
+      retryDelay: 3000,
     }),
 
     AuthModule,
