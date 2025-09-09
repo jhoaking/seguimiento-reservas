15fe7ad8f36cbe2f9706d36f8d46f1bcc4607e5c
jhoaking <peredoroj@gmail.com>
2025-09-09 10:31:04 -0400

    documentacion dtos de reservas

--- backend/src/reservas/dto/create-reserva.dto.ts
+++ backend/src/reservas/dto/create-reserva.dto.ts
@@ -1,10 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
-  IsDate,
   IsDateString,
-  IsInt,
   IsNumber,
-  IsObject,
   IsOptional,
   IsPositive,
   IsString,
@@ -13,34 +11,65 @@ import {
 } from 'class-validator';
 
 export class CreateReservaDto {
+  @ApiProperty({
+    description : 'name of the reservation',
+    example : 'reservation for a session of  professional fotographic '
+  })
   @IsString()
   @MinLength(1)
   nombre: string;
 
+  @ApiProperty({
+    description : 'status of the reservation',
+    example : 'pending'
+  })
   @IsArray()
   @IsOptional()
   @IsString({ each: true })
   estado?: string[];
 
+
+  @ApiProperty({
+    description : 'date for the reservation',
+    example : '20205-09-10'
+  })
   @IsDateString()
   fechaReserva: string;
 
+  @ApiProperty({
+    description : 'when was created the reservation'
+  })
    @IsOptional()
   @IsDateString()
   createdAt?: string;
 
+  @ApiProperty({
+    description : 'when was updated the reservation'
+  })
   @IsOptional()
   @IsDateString()
   updatedAt?: string;
 
+
+  @ApiProperty({
+    description : 'price of the reservation'
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   precio?: number;
 
+  @ApiProperty({
+    description : 'id of user for the reservation',
+    example : '0666fbc2-d5e6-4716-8a71-8f7e93824744'
+  })
   @IsUUID()
   userId:string
 
+  @ApiProperty({
+     description : 'id of service for the reservation',
+    example : '0666fbc2-d5e6-4716-8a71-8f7e93824744'
+  })
   @IsUUID()
   servicioId:string
 
--- backend/src/seed/seed.controller.ts
+++ backend/src/seed/seed.controller.ts
@@ -4,11 +4,14 @@ import {
 } from '@nestjs/common';
 
 import { SeedService } from './seed.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('seed')
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
+  @ApiResponse({status : 200 , description : 'SEED EXECUTED'})
   @Get()
   executedSeed() {
     return this.seedService.runSeed();
