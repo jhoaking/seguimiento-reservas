381675749a8835f543001f435ed3a2ae0ce19e5d
jhoaking <peredoroj@gmail.com>
2025-09-09 11:08:19 -0400

    documentacion de interaccioens

--- backend/src/interacciones/dto/create-interaccione.dto.ts
+++ backend/src/interacciones/dto/create-interaccione.dto.ts
@@ -1,18 +1,28 @@
-import { IsEnum, IsNotEmpty, IsString, MaxLength } from "class-validator";
-import { Interaccion } from "../types/interacciones";
-
-
+import { IsEnum, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { Interaccion } from '../types/interacciones';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateInteraccioneDto {
+  @ApiProperty({
+    description: 'the type that come of message ',
+    enum: Interaccion,
+    example: Interaccion.admin || Interaccion.user,
+  })
+  @IsEnum(Interaccion)
+  remitente: Interaccion;
 
+  @ApiProperty({
+    description: 'the messsage that come from n8n',
+    example: 'tell me how many services we have',
+  })
+  @IsString()
+  @MaxLength(1000)
+  contenido: string;
 
-    @IsEnum(Interaccion)
-    remitente:Interaccion
-
-    @IsString()
-    @MaxLength(1000)
-    contenido:string
-
-    @IsNotEmpty()
-    userId:string
+  @ApiProperty({
+    description: 'the id that come from n8n',
+    example: '5c41af64-f3a6-4e83-942f-e79f31eff7f8',
+  })
+  @IsNotEmpty()
+  userId: string;
 }
--- backend/src/interacciones/interacciones.controller.ts
+++ backend/src/interacciones/interacciones.controller.ts
@@ -5,7 +5,7 @@ import {
   Body,
   Param,
   ParseUUIDPipe,
-  Headers
+  Headers,
 } from '@nestjs/common';
 import { InteraccionesService } from './interacciones.service';
 import { CreateInteraccioneDto } from './dto/create-interaccione.dto';
@@ -13,13 +13,19 @@ import { Auth } from '../auth/Decorator/auth.decorator';
 import { ValidRoles } from '../auth/interface';
 import { GetUser } from '../auth/Decorator/get-user.decorator';
 import { User } from '../auth/entities/auth.entity';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Interacciones } from './entities/interaccione.entity';
 
+@ApiTags('Interactions')
 @Controller('interacciones')
 export class InteraccionesController {
   constructor(private readonly interaccionesService: InteraccionesService) {}
 
+  @ApiResponse({ status: 201, type: Interacciones })
+  @ApiResponse({ status: 400, description: 'Bad Request', type: Interacciones })
+  @ApiResponse({ status: 403, description: 'Forbidden, sender not correct' })
   @Post()
-  @Auth(ValidRoles.user , ValidRoles.ia)
+  @Auth(ValidRoles.user, ValidRoles.ia)
   create(
     @Body() createInteraccioneDto: CreateInteraccioneDto,
     @Headers('x-source') source: string,
@@ -28,15 +34,21 @@ export class InteraccionesController {
     return this.interaccionesService.create(createInteraccioneDto, fromN8n);
   }
 
+  @ApiResponse({ status: 200 })
+  @ApiResponse({ status: 400, description: 'Not found' })
+  @ApiResponse({ status: 403, description: 'Forbidden, token related' })
   @Get(':id')
-  @Auth(ValidRoles.admin )
+  @Auth(ValidRoles.admin)
   findAll(@Param('id', ParseUUIDPipe) userId: string) {
     return this.interaccionesService.findInteractionByUser(userId);
   }
 
+  @ApiResponse({ status: 200 })
+  @ApiResponse({ status: 400, description: 'Not found' })
+  @ApiResponse({ status: 403, description: 'Forbidden, token related' })
   @Get('/user')
   @Auth()
-  findAllMessagesOfUser(@GetUser() user:User){
-    return this.interaccionesService.findInteractionByUser(user.id)
+  findAllMessagesOfUser(@GetUser() user: User) {
+    return this.interaccionesService.findInteractionByUser(user.id);
   }
 }
