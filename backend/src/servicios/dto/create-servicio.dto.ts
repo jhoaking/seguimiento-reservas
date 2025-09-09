76dce10c685e325a50102ae709daa001a88f747f
jhoaking <peredoroj@gmail.com>
2025-09-09 10:09:16 -0400

    documentaion de lso dtos

--- backend/src/servicios/dto/create-servicio.dto.ts
+++ backend/src/servicios/dto/create-servicio.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsNumber,
@@ -8,22 +9,43 @@ import {
 } from 'class-validator';
 
 export class CreateServicioDto {
+
+  @ApiProperty({
+    description : 'name of the service',
+    example : 'travels'
+  })
   @IsString()
   @MinLength(1)
-  name: string;
+  name: string; 
 
+  @ApiProperty({
+    description : 'description of the service',
+    example : 'travels around the world'
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+ @ApiProperty({
+  description : 'price of  the service',
+  example : 24.4
+ })
   @IsNumber()
   @IsPositive()
   price: number;
 
+  @ApiProperty({
+    description : 'duration of the service ',
+    example : '4d'
+  })
   @IsString()
   @MinLength(1)
   duration: string;
 
+  @ApiProperty({
+    description : 'see that the service is active',
+    example : false
+  })
   @IsOptional()
   @IsBoolean()
   activo?: boolean;
